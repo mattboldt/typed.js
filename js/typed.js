7d2c74c37fef3a3f4d426958e80b47af710b3325
César Lizurey <the_dj_caesar@hotmail.com>
2016-08-25 15:03:26 +0200

    Stop blinking when typing and backspacing and resume blinking when paused

--- js/typed.js
+++ js/typed.js
@@ -137,6 +137,10 @@
 
 		// pass current string state to each function, types 1 char per call
 		typewrite: function(curString, curStrPos) {
+			// Stops blinking while typing
+			var tmpcursor = document.getElementsByClassName("typed-cursor")[0];
+			tmpcursor.style.animationPlayState = "paused";
+
 			// exit when stopped
 			if (this.stop === true) {
 				return;
@@ -199,6 +203,8 @@
 				// timeout for any pause after a character
 				self.timeout = setTimeout(function() {
 					if (curStrPos === curString.length) {
+						// Resume blinking when typing stops
+						tmpcursor.style.animationPlayState = "running";
 						// fires callback function
 						self.options.onStringTyped(self.arrayPos);
 
@@ -254,6 +260,9 @@
 		},
 
 		backspace: function(curString, curStrPos) {
+			// Stops blinking when backspacing
+			var tmpcursor = document.getElementsByClassName("typed-cursor")[0];
+			tmpcursor.style.animationPlayState = "paused";
 			// exit when stopped
 			if (this.stop === true) {
 				return;
