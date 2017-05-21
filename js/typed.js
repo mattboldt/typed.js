d42eea3ad75977be8579afaceebbfb7db7e3e69f
Richard Hall <rd.hall91@gmail.com>
2017-05-21 18:51:08 -0400

    adding declaration
    Only instance of self missing the vars declaration

--- js/typed.js
+++ js/typed.js
@@ -356,7 +356,7 @@
 
 		// Adds a CSS class to fade out current string
 		initFadeOut: function(){
-			self = this;
+			var self = this;
 			this.el.className += ' ' + this.fadeOutClass;
 			this.cursor.className += ' ' + this.fadeOutClass;
 			return setTimeout(function() {
