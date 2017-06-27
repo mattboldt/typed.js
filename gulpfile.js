d1565209afd42915186e634839658a5ce788db25
Matt Boldt <me@mattboldt.com>
2017-06-26 22:00:15 -0500

    fixed gulpfile lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,34 @@
-var x = require("babel/register"),
-    gulp = require("gulp"),
-    webpack = require("webpack-stream"),
-    sourcemaps = require("gulp-sourcemaps"),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify"),
-    livereload = require('gulp-livereload')
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
 
-gulp.task("build", function() {
-  return gulp.src("src/typed.js")
-    .pipe(webpack(require("./webpack.config.js")))
-    .pipe(gulp.dest("./lib"))
+gulp.task('build', function() {
+  return gulp.src('src/typed.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('./lib'))
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify({
-      preserveComments: "license",
+      preserveComments: 'license',
       compress: {
         /*eslint-disable */
         negate_iife: false
         /*eslint-enable */
       }
     }))
-    .pipe(rename("typed.min.js"))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("lib/"))
+    .pipe(rename('typed.min.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('lib/'))
     .pipe(livereload());
 });
 
 // Default Task
-gulp.task("default", ["build"]);
+gulp.task('default', ['build']);
 
 // Watch Task
-gulp.task("watch", function() {
+gulp.task('watch', function() {
   livereload({ start: true });
-  gulp.watch("src/*.js", ["build"]);
+  gulp.watch('src/*.js', ['build']);
 });
