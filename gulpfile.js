027fe3a84df618d58801ea25b1032a70788602d0
Matt Boldt <me@mattboldt.com>
2017-07-16 14:50:29 -0500

    bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('html-docs', () => {
   return gulp.src('./src/*.js')
     .pipe(gulpDocumentation('html'), {}, {
       name: 'Typed.js Docs',
-      version: '2.0.4'
+      version: '2.0.5'
     })
     .pipe(gulp.dest('docs'));
 });
