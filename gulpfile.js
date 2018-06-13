6f3c0686106f570d42a5b3152b789444a44dfba8
Matt Boldt <me@mattboldt.com>
2018-06-13 15:09:08 -0500

    Bumped version number to 2.0.8

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('html-docs', () => {
   return gulp.src('./src/*.js')
     .pipe(gulpDocumentation('html'), {}, {
       name: 'Typed.js Docs',
-      version: '2.0.7'
+      version: '2.0.8'
     })
     .pipe(gulp.dest('docs'));
 });
