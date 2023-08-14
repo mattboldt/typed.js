ba2ab44663aac63d1866ceacf413d8d175b10230
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-15 00:15:07 +0530

    Update pretty-print.js

--- docs/script/pretty-print.js
+++ docs/script/pretty-print.js
@@ -1,16 +1,16 @@
 (function(){
   prettyPrint();
-  var lines = document.querySelectorAll('.prettyprint.linenums li[class^="L"]');
-  for (var i = 0; i < lines.length; i++) {
+  let lines = document.querySelectorAll('.prettyprint.linenums li[class^="L"]');
+  for (let i = 0; i < lines.length; i++) {
     lines[i].id = 'lineNumber' + (i + 1);
   }
 
-  var matched = location.hash.match(/errorLines=([\d,]+)/);
+  let matched = location.hash.match(/errorLines=([\d,]+)/);
   if (matched) {
-    var lines = matched[1].split(',');
-    for (var i = 0; i < lines.length; i++) {
-      var id = '#lineNumber' + lines[i];
-      var el = document.querySelector(id);
+    let lines = matched[1].split(',');
+    for (let i = 0; i < lines.length; i++) {
+      let id = '#lineNumber' + lines[i];
+      let el = document.querySelector(id);
       el.classList.add('error-line');
     }
     return;
@@ -18,8 +18,8 @@
 
   if (location.hash) {
     // ``[ ] . ' " @`` are not valid in DOM id. so must escape these.
-    var id = location.hash.replace(/([\[\].'"@$])/g, '\\$1');
-    var line = document.querySelector(id);
+    let id = location.hash.replace(/([\[\].'"@$])/g, '\\$1');
+    let line = document.querySelector(id);
     if (line) line.classList.add('active');
   }
 })();
