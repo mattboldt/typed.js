0dceb0d5764fac46ea7e0bf8672e8e77fc4ae078
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-17 13:02:42 +0530

    Update inner-link.js

--- docs/script/inner-link.js
+++ docs/script/inner-link.js
@@ -1,17 +1,17 @@
 // inner link(#foo) can not correctly scroll, because page has fixed header,
 // so, I manually scroll.
 (function(){
-  var matched = location.hash.match(/errorLines=([\d,]+)/);
+  let matched = location.hash.match(/errorLines=([\d,]+)/);
   if (matched) return;
 
   function adjust() {
     window.scrollBy(0, -55);
-    var el = document.querySelector('.inner-link-active');
+    let el = document.querySelector('.inner-link-active');
     if (el) el.classList.remove('inner-link-active');
 
     // ``[ ] . ' " @`` are not valid in DOM id. so must escape these.
-    var id = location.hash.replace(/([\[\].'"@$])/g, '\\$1');
-    var el = document.querySelector(id);
+    let id = location.hash.replace(/([\[\].'"@$])/g, '\\$1');
+    let el = document.querySelector(id);
     if (el) el.classList.add('inner-link-active');
   }
 
@@ -23,10 +23,10 @@
 })();
 
 (function(){
-  var els = document.querySelectorAll('[href^="#"]');
-  var href = location.href.replace(/#.*$/, ''); // remove existed hash
-  for (var i = 0; i < els.length; i++) {
-    var el = els[i];
+  let els = document.querySelectorAll('[href^="#"]');
+  let href = location.href.replace(/#.*$/, ''); // remove existed hash
+  for (let i = 0; i < els.length; i++) {
+    let el = els[i];
     el.href = href + el.getAttribute('href'); // because el.href is absolute path
   }
 })();
