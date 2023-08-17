00dc687d92bb24629506eeb8d2477e09efa95e3b
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-17 13:01:17 +0530

    Update manual.js

--- docs/script/manual.js
+++ docs/script/manual.js
@@ -1,12 +1,12 @@
 (function(){
-  var matched = location.pathname.match(/\/(manual\/.*\.html)$/);
+  let matched = location.pathname.match(/\/(manual\/.*\.html)$/);
   if (!matched) return;
 
-  var currentName = matched[1];
-  var cssClass = '.navigation .manual-toc li[data-link="' + currentName + '"]';
-  var styleText = cssClass + '{ display: block; }\n';
+  let currentName = matched[1];
+  let cssClass = '.navigation .manual-toc li[data-link="' + currentName + '"]';
+  let styleText = cssClass + '{ display: block; }\n';
   styleText += cssClass + '.indent-h1 a { color: #039BE5 }';
-  var style = document.createElement('style');
+  let style = document.createElement('style');
   style.textContent = styleText;
   document.querySelector('head').appendChild(style);
 })();
