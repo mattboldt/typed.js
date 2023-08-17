f88f1c2698ff8806a86c19b0b2ea544e59c51d6d
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-17 13:00:12 +0530

    Update patch-for-local.js

--- docs/script/patch-for-local.js
+++ docs/script/patch-for-local.js
@@ -1,7 +1,7 @@
 (function(){
   if (location.protocol === 'file:') {
-    var elms = document.querySelectorAll('a[href="./"]');
-    for (var i = 0; i < elms.length; i++) {
+    let elms = document.querySelectorAll('a[href="./"]');
+    for (let i = 0; i < elms.length; i++) {
       elms[i].href = './index.html';
     }
   }
